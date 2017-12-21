dba91a9c70735b72a2b472bada1889dcf138e89c
Brian Mize <brian.r.mize@eagles.ewu.edu>
2017-12-20 16:32:18 -0800

    Remove unused usings

--- VR_test_wpf/Child1.xaml.cs
+++ VR_test_wpf/Child1.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using WindowsInput;
-using WindowsInput.Native;
 
 namespace VR_test_wpf
 {
